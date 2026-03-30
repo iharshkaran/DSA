477a75e4c07e0ee77b674991545bb636e485ef42
HARSH <harshkaranofficial@gmail.com>
2026-03-30 18:11:15 +0530

    Overriding

--- 13_OOPs/11_overriding.cpp
+++ 13_OOPs/11_overriding.cpp
@@ -1,5 +1,31 @@
 #include<iostream>
 using namespace std;
+class Scooty{
+public:
+    int topSpeed;
+    float mileage;
+    virtual void sound(){
+        cout<<"Vroom Vroom"<<endl;
+    }
+private:
+    int bootSpace;
+};
+class Bike : public Scooty{
+public:
+    void sound(){
+        cout<<"Dhroom Dhroom"<<endl;
+    }
+};
+class SuperBike : public Scooty{
+public:
+    void sound(){
+        cout<<"Zroom Zroom"<<endl;
+    }
+};
 int main(){
-    
+    Scooty* x = new Bike(); //Object Pointer
+    x->sound(); // Dynamic Polymorphism
+
+    Scooty* s = new SuperBike();
+    s->sound();
 }

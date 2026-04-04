4e9ac2ea2d5fc71f39e397e15148e9a76066e70c
HARSH <harshkaranofficial@gmail.com>
2026-04-04 15:37:44 +0530

    Maze Path

--- 07_Recursion/10_mazePath.cpp
+++ 07_Recursion/10_mazePath.cpp
@@ -8,6 +8,14 @@ int maze(int sr, int sc, int er, int ec){
     return rightWays + downWays;
     
 }
+int maze2(int row, int col){
+    if(row<1 || col<1) return 0;
+    if(row==1 && col==1) return 1;
+    int rightWays = maze2(row,col-1);
+    int downWays = maze2(row-1,col);
+    return rightWays + downWays;
+    
+}
 void printPath(int sr, int sc, int er, int ec, string s){
     if(sr>er || sc>ec) return;
     if(sr==er && sc==ec){ // Destination Reached
@@ -15,9 +23,22 @@ void printPath(int sr, int sc, int er, int ec, string s){
         return;
     }
     printPath(sr,sc+1,er,ec,s+"R"); //Right
-    printPath(sr+1,sc,er,ec,s+"D");  //Down 
+    printPath(sr+1,sc,er,ec,s+"D"); //Down 
+}
+void printPath2(int row, int col, string s){
+    if(row<1 || col<1) return;
+    if(row==1 && col==1){
+        cout<<s<<endl;
+        return;
+    }
+    printPath2(row,col-1,s+"R"); //Right
+    printPath2(row-1,col,s+"D"); //Down   
 }
 int main(){
-    cout<<maze(1,1,4,4)<<endl;
+    cout<<"Maze (4 Parameter) = "<<maze(1,1,4,4)<<endl;
+    cout<<"Maze (2 Parameter) = "<<maze2(4,4)<<endl<<endl;
+    cout<<"Paths (4 Parameter):"<<endl;
     printPath(1,1,4,4,"");
+    cout<<endl<<"Paths (2 Parameter):"<<endl;
+    printPath2(4,4,"");
 }

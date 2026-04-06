be6bb504d7fce54cce233c790d93aab6a7f0d624
HARSH <harshkaranofficial@gmail.com>
2026-04-06 22:11:34 +0530

    Merge Sort

--- 08_advSortings/01_MergeSort/01_mergeSortCode.cpp
+++ 08_advSortings/01_MergeSort/01_mergeSortCode.cpp
@@ -6,33 +6,41 @@ void merge(vector<int> &a, vector<int> &b, vector<int> &res){
     int j = 0; //b
     int k = 0; //res
     while(i<a.size() && j<b.size()){
-        if(a[i]<b[j]){
-            res[k++]=a[i++];
-        } else {
-            res[k++]=b[j++];
-        }
+        if(a[i]<=b[j]) res[k++]=a[i++]; 
+        else res[k++]=b[j++];  
     }
     if(i==a.size()){
-        while(j<b.size()){
+        while(j<b.size())
             res[k++]=b[j++];
-        }
     }
     if(j==b.size()){
-        while(i<a.size()){
+        while(i<a.size())
             res[k++]=a[i++];
-        }
     }
 }
+void mergeSort(vector<int> &v){
+    int n = v.size();
+    if(n==1) return;
+    int n1 = n/2 , n2 = n-n/2;
+    vector<int> a(n1), b(n2);
+    // copy pasting
+    for(int i=0; i<n; i++)
+        a[i]=v[i];
+    for(int i=0; i<n; i++)
+        b[i]=v[i+n1];
+    //Recursion
+    mergeSort(a);
+    mergeSort(b);
+    //merge
+    merge(a,b,v);
+
+}
 int main(){
-    int arr1[]={1,2,3,4};
-    int arr2[]={5,6,7,8,9,10};
-    int n1 = sizeof(arr1)/sizeof(arr1[0]);
-    int n2 = sizeof(arr2)/sizeof(arr2[0]);
-    vector<int> a(arr1,arr1+n1);
-    vector<int> b(arr2,arr2+n2);
-    vector<int> res(n1+n2);
-    merge(a,b,res);
-    for(int i=0; i<res.size(); i++){
-        cout<<res[i]<<" ";
+    int arr[]={10,2,6,7,8,9,5,1,3,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr,arr+n);
+    mergeSort(v);
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
     }
 }
